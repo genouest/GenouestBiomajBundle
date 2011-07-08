299d127f6c81440176d79ab1e4f949c1a9ad9d1c
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2011-07-08 14:20:13 +0200

    validators are available as services, bankmanager accessed using the service

--- Constraints/BiomajValidator.php
+++ Constraints/BiomajValidator.php
@@ -1,26 +1,42 @@
 <?php
 
+/*
+ * Copyright 2011 Anthony Bretaudeau <abretaud@irisa.fr>
+ *
+ * Licensed under the CeCILL License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
 namespace Genouest\Bundle\BiomajBundle\Constraints;
 
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\HttpFoundation\Request;
-
-use Genouest\Bundle\BiomajBundle\Biomaj\BankManagerAccessor;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * BiomajValidator validates that the value is one of the available banks.
  */
 class BiomajValidator extends ConstraintValidator
 {
+    protected $container;
+    
+    public function __construct(ContainerInterface $container) {
+        $this->container = $container;
+    }
+
     public function isValid($value, Constraint $constraint)
     {
         if (null === $value) {
             return true;
         }
         
-        $bankaccessor = new BankManagerAccessor();
-        $choices = $this->getPathArray($bankaccessor->prepareBankList($constraint->type, $constraint->format, $constraint->cleanup));
+        $bankManager = $this->container->get('biomaj.bank.manager');
+        $choices = $this->getPathArray($bankManager->getBankList($constraint->type, $constraint->format, $constraint->cleanup));
 
         if (!is_array($choices)) {
             throw new ConstraintDefinitionException('The list of banks couldn\'t be generated');
